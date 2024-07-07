703e6ac4936aa9f2ac2260bb4e9c90cbeeb9bd7f
user <user@DESKTOP-UHS5Q6S>
2024-07-07 16:26:23 +0530

    second commit

---
+++ corejavaproject/src/introduction/wideningexample.java
@@ -0,0 +1,11 @@
+package introduction;
+
+public class wideningexample {
+	public static void main(String[] args) {
+		int X=7;
+		float Y=X;
+		System.out.println("before modification" +X);
+		System.out.println("after modification"+Y);
+	}
+
+}
