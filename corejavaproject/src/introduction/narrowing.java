3f0715feba68a86db053f0fc1ed03894cef42a23
user <user@DESKTOP-UHS5Q6S>
2024-07-07 16:46:33 +0530

    Third commit

---
+++ corejavaproject/src/introduction/narrowing.java
@@ -0,0 +1,11 @@
+package introduction;
+
+public class narrowing {
+	public static void main(String[] args) {
+		double d=8.9;
+		int i=(int)d;
+		System.out.println("before modification"+d);
+		System.out.println("after modification"+i);
+	}
+
+}
